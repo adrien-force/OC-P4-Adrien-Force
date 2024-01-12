e538597f2a2bf4cd9c40e96ba834f827ce02a058
Adrien F <force.adri@gmail.com>
2024-01-12 14:45:25 +0100

    traitement de l'ajout d'eouvre

--- traitement.php
+++ traitement.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'bdd.php';
+
 $postData = $_POST;
 
 //check if form was submitted
@@ -30,12 +32,24 @@ else {
     $artiste = htmlspecialchars($postData['artiste']);
     $image = htmlspecialchars($postData['image']);
     $description = htmlspecialchars($postData['description']);
+
+    $sql = "INSERT INTO oeuvres (titre, artiste, image, description) VALUES (:titre, :artiste, :image, :description)";
+    $statement = $mysqlClient->prepare($sql);
+    $statement->execute([
+        ':titre' => $titre,
+        ':artiste' => $artiste,
+        ':image' => $image,
+        ':description' => $description
+    ]);
+    header("refresh:5;url=index.php"); 
+    require 'header.php';
+    ?>
+
+    <h1>Bravo !</h1>
+    <p>Votre oeuvre a bien été ajoutée à la base de données.</p>
+    <p>Vous allez être redirigé vers la page d'accueil dans 5 secondes.</p>
+
+
+<?php 
 }
-
-
-
-
-
-
-
 ?>
